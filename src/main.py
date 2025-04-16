92fa962369fd5a98c1e28aa69f8503bbadb7ab59
GustavoCunha <gustavo_developer@outlook.com>
2025-04-16 07:31:00 -0300

    alterando-funções

--- src/main.py
+++ src/main.py
@@ -1,11 +1,2 @@
 
 
-import tkinter as tk
-from interface.telas import tela_inicial
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    root.withdraw()  # Oculta a janela principal
-    tela_inicial()
-    root.mainloop()
-
