dae24c32feea4fd2a7830512763ea563e978ddd3
Willami Bezerra <willamibezerra035@gmail.com>
2025-05-04 16:11:02 -0300

    corrigi arquitetura

--- src/repositories/usuario_repository.py
+++ src/repositories/usuario_repository.py
@@ -1,26 +1,23 @@
-from database.banco import conectar
-
-def cadastrar_usuario(nome, email, senha):
+from database.conection_livros import conectar
+def inserir_usuario(nome, email, senha):
     conn = conectar()
     cursor = conn.cursor()
-
     try:
         cursor.execute("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)", (nome, email, senha))
         conn.commit()
-        print("Usuário cadastrado com sucesso!")
         return True
     except Exception as e:
-        print("Erro ao cadastrar usuário:", e)
+        print("Erro ao inserir usuário:", e)
         return False
     finally:
         conn.close()
 
-def verificar_login(email, senha):
+def buscar_usuario_por_email_senha(email, senha):
     conn = conectar()
     cursor = conn.cursor()
 
     cursor.execute("SELECT id, nome FROM usuarios WHERE email = ? AND senha = ?", (email, senha))
     usuario = cursor.fetchone()
-
+    
     conn.close()
-    return usuario  # Retorna (id, nome) se encontrado, ou None
+    return usuario  # (id, nome) ou None
